eb651ca41fe908b8ab86ff946ef0c399991b9047
Harsh-2706 <harsh.bubna06@gmail.com>
2026-03-20 10:17:44 +0530

    uc1 commit

---
+++ App/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> consist = new ArrayList<>();
+
+        System.out.println("Initial Bogie Count: " + consist.size());
+    }
+}
